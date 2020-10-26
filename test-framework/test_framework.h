bd9cfb8937db5b599abffce8e15216ddfa468e0e
Afrizal <afrizal2703@gmail.com>
2020-10-26 20:50:47 +0700

    Initial Commit

---
+++ test-framework/test_framework.h
@@ -0,0 +1,19 @@
+//
+//  test_framework.h
+//  test-framework
+//
+//  Created by afrizal on 26/10/20.
+//  Copyright © 2020 Afrizal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_framework.
+FOUNDATION_EXPORT double test_frameworkVersionNumber;
+
+//! Project version string for test_framework.
+FOUNDATION_EXPORT const unsigned char test_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_framework/PublicHeader.h>
+
+
